45d577eea3962ef8bed10e1a8d2c2420bf886d87
Toby Atkin-Wright <toby@filmgold.com>
2009-05-19 16:15:13 +0100

    Turned off PHP output buffer, and added flush() commands.

--- lj-friends-on-dw/index.php
+++ lj-friends-on-dw/index.php
@@ -1,4 +1,5 @@
 <?php
+ob_end_flush();
 $user = isset($_GET['user']) ? $_GET['user'] : '';
 ?><?xml version="1.0" encoding="UTF-8" standalone="no" ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -79,6 +80,7 @@ if ($user != "")
 			}
 			
 			print "</li>\n";
+			flush();
 		}
 		print "</ul>\n";
 	}
